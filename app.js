670c589523c1de3eeec72e39100a71c242e84038
chava <c7193394@gmail.com>
2021-10-15 11:07:40 +0300

    15

--- app.js
+++ app.js
@@ -19,14 +19,15 @@ app.all('*', function (req, res, next) {
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
 
-app.use((req, res, next) => {
-  next(createError(404));
-});
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
